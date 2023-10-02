bfe14aa72947dce2c459c5b4861f1e6591143c65
박규민 <131234408+kyumin7487@users.noreply.github.com>
2023-10-02 20:47:00 +0900

    틀린부분 수정

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,11 +7,11 @@ export default function start(){
            <h1>kyumin</h1>
            <nav className="nav">
                <ul>
-                   <li><a herf="about.js">About</a></li>
-                   <li><a herf="portfolio.js">PortFolio</a></li>
-                   <li><a herf="contact.js">Contact</a></li>
-                   <li><a herf="project.js">Project</a></li>
-                   <li><a herf="activity.js">Activity</a></li>
+                   <li><a href="about.js">About</a></li>
+                   <li><a href="portfolio.js">PortFolio</a></li>
+                   <li><a href="contact.js">Contact</a></li>
+                   <li><a href="project.js">Project</a></li>
+                   <li><a href="activity.js">Activity</a></li>
                </ul>
            </nav>
        </header>
