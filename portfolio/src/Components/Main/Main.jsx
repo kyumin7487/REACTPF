8afa841777630f6e946cf554c7abcef285125154
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-01-02 21:10:49 +0900

    :sparkles: Feature:: 이름타이틀

--- portfolio/src/Components/Main/Main.jsx
+++ portfolio/src/Components/Main/Main.jsx
@@ -4,7 +4,9 @@ export default function Main() {
     return (
         <div className='MainPage'>
             <Head />
-            <div className='Background'></div>
+            <div className='Background'>
+                <div className='NameTitle'>새싹 같은 개발자<br></br>박규민입니다.</div>
+            </div>
         </div>
     )
 }
